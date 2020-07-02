bee2c21c691f23e7c9c5fd2b9761ae73f84fc45a
llinshui <1607732696@qq.com>
2020-07-02 11:00:28 +0800

    补充UR5

--- Assets/UR10/Scripts/ControlUI.cs
+++ Assets/UR10/Scripts/ControlUI.cs
@@ -6,11 +6,11 @@ using UnityEngine.UI;
 public class ControlUI : MonoBehaviour
 {
     GameObject robot, mode;
-    GameObject[] controlModes = new GameObject[6];
+    GameObject[] controlModes = new GameObject[9];//7.2添加观察臂
     int robot_index = 0;
     int mode_index = -1;
 
-    GameObject[] controlRobots = new GameObject[2];
+    GameObject[] controlRobots = new GameObject[3];//7.2添加观察臂
     GameObject[] ChoseModes = new GameObject[3];
     GameObject activeMode,activeRobot,activeAA;
     // Start is called before the first frame update
@@ -20,10 +20,13 @@ public class ControlUI : MonoBehaviour
         mode = GameObject.Find("模式选择");
         controlModes[0] = this.transform.Find("离线任务-主").gameObject;
         controlModes[1] = this.transform.Find("离线任务-从").gameObject;
-        controlModes[2] = this.transform.Find("单轴点动-主").gameObject;
-        controlModes[3] = this.transform.Find("单轴点动-从").gameObject;
-        controlModes[4] = this.transform.Find("末端联动-主").gameObject;
-        controlModes[5] = this.transform.Find("末端联动-从").gameObject;
+        controlModes[2] = this.transform.Find("离线任务-观察臂").gameObject;
+        controlModes[3] = this.transform.Find("单轴点动-主").gameObject;
+        controlModes[4] = this.transform.Find("单轴点动-从").gameObject;
+        controlModes[5] = this.transform.Find("末端联动-主").gameObject;
+        controlModes[6] = this.transform.Find("末端联动-从").gameObject;
+        controlModes[7] = this.transform.Find("单轴点动-观察臂").gameObject;
+        controlModes[8] = this.transform.Find("末端联动-观察臂").gameObject;
         activeMode = null;
         activeRobot = null;
         activeAA = null;
@@ -60,7 +63,7 @@ public class ControlUI : MonoBehaviour
         mode_index = i;
         if(activeMode==null)
         {
-            activeMode = controlModes[robot_index + mode_index * 2];
+            activeMode = controlModes[robot_index + mode_index * 3];//7.2添加观察臂
         }
         else
         {
@@ -76,13 +79,14 @@ public class ControlUI : MonoBehaviour
         }
         activeAA = ChoseModes[mode_index];
         activeAA.GetComponent<Image>().color = Color.red;
-        activeMode = controlModes[robot_index + mode_index * 2];
+        activeMode = controlModes[robot_index + mode_index * 3];//7.2添加观察臂
         activeMode.SetActive(true);
     }
     void Init()
     {
         controlRobots[0] = robot.transform.Find("Primary").gameObject;
         controlRobots[1] = robot.transform.Find("Secondary").gameObject;
+        controlRobots[2] = robot.transform.Find("Observation").gameObject;
         ChoseModes[0] = mode.transform.Find("离线任务").gameObject;
         ChoseModes[1] = mode.transform.Find("单轴点动").gameObject;
         ChoseModes[2] = mode.transform.Find("末端联动").gameObject;
